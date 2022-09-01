08dbfaa820140745e81e85dfeda80fac284ab829
hwz <2380227583@qq.com>
2022-09-01 13:36:54 +0800

    电影详情页

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+	<Xiangqing></Xiangqing>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Xiangqing from './components/Xiangqing.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
+	Xiangqing
   }
 }
 </script>
@@ -23,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
